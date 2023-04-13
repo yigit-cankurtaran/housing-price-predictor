8f45e2c7639c72ffba5853d4165f6827e6e9f348
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2023-04-13 22:51:54 +0300

    Removed unused imports.

--- explore.py
+++ explore.py
@@ -1,11 +1,5 @@
 from sklearn.datasets import fetch_california_housing
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 
 # loading the data
 housing = fetch_california_housing()
