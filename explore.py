dac1ab8ee95633be085d31c38cd4863a23a94d0a
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2023-04-13 22:51:14 +0300

    Finished exploring the dataset, now moving on to visualization.

--- explore.py
+++ explore.py
@@ -12,5 +12,17 @@ housing = fetch_california_housing()
 
 # export the data into a pandas dataframe
 df = pd.DataFrame(housing.data, columns=housing.feature_names)
+
+# exploring the data
 print(f'The shape of the data is: {df.shape}')
 print(df.head())
+print(df.describe())
+# df.describe() gives us the mean, std, min, max, 25%, 50%, 75% of the data
+
+print(df.dtypes)
+# df.dtypes gives us the data types of each column
+# in this dataset, all the columns are float64
+
+# checking for missing values
+print(df.isnull().sum())
+# no missing values
