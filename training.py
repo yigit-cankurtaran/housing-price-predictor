11e3ad7576304d4ea3c55e201ec9a6c63a1bcde1
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2023-04-14 00:19:01 +0300

    Added in functionality for the model. 57% accuracy, will improve.

--- training.py
+++ training.py
@@ -17,3 +17,30 @@ X = df
 y = housing.target
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42)
+
+# train the model
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# evaluate the model
+print(f'The model\'s R^2 score is: {model.score(X_test, y_test)}')
+
+# visualize the model's predictions
+y_pred = model.predict(X_test)
+plt.scatter(y_test, y_pred)
+plt.xlabel('Actual Values')
+plt.ylabel('Predicted Values')
+plt.show()
+
+# visualize the model's residuals
+residuals = y_test - y_pred
+plt.scatter(y_pred, residuals)
+plt.xlabel('Predicted Values')
+plt.ylabel('Residuals')
+plt.show()
+
+# visualize the model's residuals' distribution
+plt.hist(residuals, bins=50)
+plt.xlabel('Residuals')
+plt.ylabel('Frequency')
+plt.show()
