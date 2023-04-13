204fbb6aaf194bb5527e2987850faace26daacc4
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2023-04-14 01:20:00 +0300

    Removed unused libs.

--- training.py
+++ training.py
@@ -3,7 +3,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import Ridge
 import numpy as np
 from sklearn.metrics import mean_squared_error
 from sklearn.ensemble import RandomForestRegressor
